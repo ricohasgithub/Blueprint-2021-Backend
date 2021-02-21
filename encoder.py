966bb391a93708bec1138d8e0c6bbaea617f0a69
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-21 10:10:23 -0500

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -2,9 +2,23 @@
 # Universal sentence encoder
 import tensorflow_hub as hub
 
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-embeddings = embed([
-    "The quick brown fox jumps over the lazy dog.",
-    "I am a sentence for which I would like to get its embedding"])
+class Universal_Sentence_Encoder():
 
-print(embeddings)
+    def __init__(self, sentences):
+        embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
+        embeddings = embed([
+            "The quick brown fox jumps over the lazy dog.",
+            "I am a sentence for which I would like to get its embedding"])
+
+    def get_semantic_similarity(self, labels, features, rotation):
+        corr = np.inner(features, features)
+        sns.set(font_scale=1.2)
+        g = sns.heatmap(
+            corr,
+            xticklabels=labels,
+            yticklabels=labels,
+            vmin=0,
+            vmax=1,
+            cmap="YlOrRd")
+        g.set_xticklabels(labels, rotation=rotation)
+        g.set_title("Semantic Textual Similarity")

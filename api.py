2829e06ef28dd6acf18a57f6848114730e43adef
CarolineBaillie <caroline@bailliefamily.com>
2021-02-21 09:07:14 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,10 +1,21 @@
 from flask import Flask, request, jsonify
+from cs50 import SQL
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def ml_func():
-    return "Hello World!"
+    # call to ml should go here
+    resp = False
+    return jsonify({'data' : resp})
+
+@app.route('/save')
+def save_db():
+    # call to ml should go here
+    resp = False
+    return jsonify({'data' : resp})
 
 if __name__ == '__main__':
     app.run()

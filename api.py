284bfae13710907f7aa6fb83df553b4e37c1c408
CarolineBaillie <caroline@bailliefamily.com>
2021-02-21 11:56:42 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,16 +1,32 @@
 from flask import Flask, request, jsonify
 from cs50 import SQL
+from flask_cors import CORS, cross_origin
+import json
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 #config db
 db = SQL("sqlite:///quiz.db")
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def ml_func():
+    if request.method == "POST":
+        print("run")
+        text = json.loads(request.data)
+        text = text['data']
+        # text = request.json['data']
     # call to ml should go here
-    resp = False
+    print(text)
+    resp = "SUCCESS!"
+    # should save actually go after ml call?
+    return jsonify({'data' : resp})
+
+@app.route('/g', methods=['GET'])
+def get():
+    resp = "SUCCESS!"
     # should save actually go after ml call?
     return jsonify({'data' : resp})
 
@@ -23,4 +39,4 @@ def save_db():
     return
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

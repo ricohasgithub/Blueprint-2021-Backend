11d5441d194f150d342ab8122209c3bb2ad1e59d
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-21 09:48:21 -0500

    Bug Patch 2.0

--- spacy_nn.py
+++ spacy_nn.py
@@ -10,8 +10,8 @@ class Spacy_NN():
         # Process whole documents
         self.text_in = text
         self.doc = nlp(self.text_in)
-        self.nouns = [chunk.text for chunk in doc.noun_chunks]
-        self.verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
+        self.nouns = [chunk.text for chunk in self.doc.noun_chunks]
+        self.verbs = [token.lemma_ for token in self.doc if token.pos_ == "VERB"]
 
     def get_nouns(self):
         return nouns
@@ -20,5 +20,5 @@ class Spacy_NN():
         return verbs
 
     def print_lem(self):
-        for entity in selfdoc.ents:
+        for entity in self.doc.ents:
             print(entity.text, entity.label_)

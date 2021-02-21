1fbb831f0429134022caa06586806bfedb5670ec
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-21 09:49:19 -0500

    Restructured Instance Variables

--- spacy_nn.py
+++ spacy_nn.py
@@ -1,15 +1,13 @@
 
 import spacy
 
-# Load English tokenizer, tagger, parser, NER and word vectors
-nlp = spacy.load("en_core_web_sm")
-
 class Spacy_NN():
 
     def __init__(self, text):
-        # Process whole documents
+        # Load English tokenizer, tagger, parser, NER and word vectors
+        self.nlp = spacy.load("en_core_web_sm")
         self.text_in = text
-        self.doc = nlp(self.text_in)
+        self.doc = self.nlp(self.text_in)
         self.nouns = [chunk.text for chunk in self.doc.noun_chunks]
         self.verbs = [token.lemma_ for token in self.doc if token.pos_ == "VERB"]
 

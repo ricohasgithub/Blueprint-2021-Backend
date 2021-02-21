972678d1af0859d7a5136b792d58a86835894c7e
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-21 10:05:11 -0500

    Updated Universal Sentence Encoder

--- question_gen.py
+++ question_gen.py
@@ -1,8 +1,7 @@
 
-# Ask 3 types of questions:
+# Ask 2 types of questions:
 #       1. Fill in the blank --> use spaCy NLP to find "keywords" to remove
 #       2. What/When/Where --> use intent extraction and sentence similarity to compare answers
-#       3. 
 
 from spacy_nn import Spacy_NN
 

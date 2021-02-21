78c611d5f39be78f13ea700105c83b589f26eede
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-21 09:47:48 -0500

    Bug Patch with Spacy_NN

--- spacy_nn.py
+++ spacy_nn.py
@@ -4,20 +4,14 @@ import spacy
 # Load English tokenizer, tagger, parser, NER and word vectors
 nlp = spacy.load("en_core_web_sm")
 
-class spacy_nn():
+class Spacy_NN():
 
     def __init__(self, text):
         # Process whole documents
-        self.text = ("When Sebastian Thrun started working on self-driving cars at "
-            "Google in 2007, few people outside of the company took him "
-            "seriously. “I can tell you very senior CEOs of major American "
-            "car companies would shake my hand and turn away because I wasn’t "
-            "worth talking to,” said Thrun, in an interview with Recode earlier "
-            "this week.")
-        self.doc = nlp(text)
+        self.text_in = text
+        self.doc = nlp(self.text_in)
         self.nouns = [chunk.text for chunk in doc.noun_chunks]
         self.verbs = [token.lemma_ for token in doc if token.pos_ == "VERB"]
-
 
     def get_nouns(self):
         return nouns
@@ -26,5 +20,5 @@ class spacy_nn():
         return verbs
 
     def print_lem(self):
-        for entity in doc.ents:
+        for entity in selfdoc.ents:
             print(entity.text, entity.label_)
